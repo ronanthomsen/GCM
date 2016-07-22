82f82d40221f14b8d9db5e44e20e72ebaf39b98b
ronanthomsen <ronanabdo@gmail.com>
2016-07-22 16:32:48 -0300

    First Commit

---
+++ app/src/main/java/gcm/play/android/samples/com/gcmquickstart/QuickstartPreferences.java
@@ -0,0 +1,11 @@
+package gcm.play.android.samples.com.gcmquickstart;
+
+/**
+ * Created by PF0325 on 12/06/2016.
+ */
+public class QuickstartPreferences {
+
+    public static final String SENT_TOKEN_TO_SERVER = "sentTokenToServer";
+    public static final String REGISTRATION_COMPLETE = "registrationComplete";
+
+}
